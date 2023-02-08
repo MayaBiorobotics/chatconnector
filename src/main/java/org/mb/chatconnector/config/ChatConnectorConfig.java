edf5349d8e7a6d7b19d9b712a5d4350f3c6c25dd
VladislavAnanyev <a.vlad.v@yandex.ru>
2023-02-09 01:35:50 +0300

    fix

--- src/main/java/org/mb/chatconnector/config/ChatConnectorConfig.java
+++ src/main/java/org/mb/chatconnector/config/ChatConnectorConfig.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ObjectMapperConfig {
+public class ChatConnectorConfig {
 
     @Bean
     public ChatConnectorProperties chatConnectorProperties() {
