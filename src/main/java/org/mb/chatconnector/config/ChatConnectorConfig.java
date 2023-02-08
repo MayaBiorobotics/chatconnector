4e671da25adc9ad38dca360262fb8661a11ece16
VladislavAnanyev <a.vlad.v@yandex.ru>
2023-02-09 01:35:50 +0300

    fix

--- src/main/java/org/mb/chatconnector/config/ChatConnectorConfig.java
+++ src/main/java/org/mb/chatconnector/config/ChatConnectorConfig.java
@@ -1,11 +1,10 @@
 package org.mb.chatconnector.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class ObjectMapperConfig {
+public class ChatConnectorConfig {
 
     @Bean
     public ChatConnectorProperties chatConnectorProperties() {
